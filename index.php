6889b7121a3e0a66b30edea4f354b7ebb61d3e0b
miihelene <helene.papin62@gmail.com>
2016-02-08 16:56:31 +0100

    CSS JS File delete - Rename Doc IMG - HTML CSS JS Add portfolio

---
+++ index.php
@@ -0,0 +1,398 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>MiinowChan</title>
+  <meta name="description" content="">
+  <meta name="author" content="MiinowChan">
+  <meta name="keywords" content=""/>
+  <link rel="icon" href="../../favicon.ico">
+
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- My CSS -->
+  <link rel="stylesheet" href="css/customstyle.css">
+	<link rel="stylesheet" href="css/customportfolio.css" />
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]-->
+
+  <!--[endif]-->
+</head>
+<body>
+  <!-- ==================================== LOADING WRAP ==================================== -->
+
+
+  <div class="preloader" id="preloader">
+      <div class="item">
+        <img src="img/loading.gif">
+      </div>
+  </div>
+
+<!-- ==================================== HOME WRAP ==================================== -->
+  <div class="homewrap" id="homewrap">
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><span>M</span>iinow <span>C</span>han</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#homewrap">Accueil</a></li>
+            <li><a href="#portfoliowrap">Portfolio</a></li>
+            <li><a href="#aboutwrap">À propos</a></li>
+            <li><a href="#contactwrap">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav><!-- /nav -->
+  </div><!-- homewrap -->
+
+<!-- ================================== PORTFOLIO WRAP ================================== -->
+  <div class="portfoliowrap" id="portfoliowrap">
+    <div class="container">
+
+      <div class="col-md-12 sub-title">
+        <h2 class="title-lv2 title-green">Portfolio</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <a href="#">+ d'info</a></p>
+      </div>
+
+      <div class="col-md-12 sub-nav">
+        <ul id="portfolio-filter">
+          <li class="current"><a href="#all" title="">All</a></li>
+          <li><a href="#logo" title="" rel="logo">Logo</a></li>
+          <li><a href="#documenet" title="" rel="document">Document</a></li>
+          <li><a href="#site" title="" rel="site">Site</a></li>
+          <li><a href="#illustration" title="" rel="illustration">Illustration</a></li>
+          <li><a href="#perso" title="" rel="perso">Perso</a></li>
+        </ul>
+      </div>
+
+      <div class="text-center">
+
+          <section class="col-lg-12" id="portfolio">
+                <ul class="grid">
+                  <!-- 1 item portoflio-->
+                  <li>
+                    <a href="#">
+                      <img src="img/portfolio/1.png" alt="Portfolio item" />
+                        <div class="text">
+                          <p>PORTFOLIO 1</p>
+                          <p class="description">Your text here...</p>
+                      </div>
+                    </a>
+                  </li>
+
+                  <!-- 2 item portoflio-->
+                  <li>
+                    <a href="#">
+                      <img src="img/portfolio/2.png" alt="Portfolio item" />
+                        <div class="text">
+                          <p>PORTFOLIO 1</p>
+                          <p class="description">Your text here...</p>
+                      </div>
+                    </a>
+                  </li>
+
+                  <!-- 3 item portoflio-->
+                  <li>
+                    <a href="#">
+                      <img src="img/portfolio/3.png" alt="Portfolio item" />
+                        <div class="text">
+                          <p>PORTFOLIO 1</p>
+                          <p class="description">Your text here...</p>
+                      </div>
+                    </a>
+                  </li>
+
+                  <!-- 4 item portoflio-->
+                  <li>
+                    <a href="#">
+                      <img src="img/portfolio/4.png" alt="Portfolio item" />
+                        <div class="text">
+                          <p>PORTFOLIO 1</p>
+                          <p class="description">Your text here...</p>
+                      </div>
+                    </a>
+                  </li>
+
+                  <!-- 5 item portoflio-->
+                  <li>
+                    <a href="#">
+                      <img src="img/portfolio/5.jpeg" alt="Portfolio item" />
+                        <div class="text">
+                          <p>PORTFOLIO 1</p>
+                          <p class="description">Your text here...</p>
+                      </div>
+                    </a>
+                  </li>
+
+                  <!-- 6 item portoflio-->
+                  <li>
+                    <a href="#">
+                      <img src="img/portfolio/6.png" alt="Portfolio item" />
+                        <div class="text">
+                          <p>PORTFOLIO 1</p>
+                          <p class="description">Your text here...</p>
+                      </div>
+                    </a>
+                  </li>
+
+                 <!-- 7 item portoflio-->
+                 <li>
+                   <a href="#">
+                     <img src="img/portfolio/7.png" alt="Portfolio item" />
+                       <div class="text">
+                         <p>PORTFOLIO 1</p>
+                         <p class="description">Your text here...</p>
+                     </div>
+                   </a>
+                 </li>
+
+                  <!-- 8 item portoflio-->
+                  <li>
+                    <a href="#">
+                      <img src="img/portfolio/8.png" alt="Portfolio item" />
+                        <div class="text">
+                          <p>PORTFOLIO 1</p>
+                          <p class="description">Your text here...</p>
+                      </div>
+                    </a>
+                  </li>
+
+                  <!-- 9 item portoflio-->
+                  <li>
+                    <a href="#">
+                      <img src="img/portfolio/9.png" alt="Portfolio item" />
+                        <div class="text">
+                          <p>PORTFOLIO 1</p>
+                          <p class="description">Your text here...</p>
+                      </div>
+                    </a>
+                  </li>
+
+                  <!-- 10 item portoflio-->
+                  <li>
+                    <a href="#">
+                      <img src="img/portfolio/10.png" alt="Portfolio item" />
+                        <div class="text">
+                          <p>PORTFOLIO 1</p>
+                          <p class="description">Your text here...</p>
+                      </div>
+                    </a>
+                  </li>
+
+                  <!-- 11 item portoflio-->
+                  <li>
+                    <a href="#">
+                      <img src="img/portfolio/11.png" alt="Portfolio item" />
+                        <div class="text">
+                          <p>PORTFOLIO 11</p>
+                          <p class="description">Your text here...</p>
+                      </div>
+                    </a>
+                  </li>
+
+
+                </ul>
+           <a href="#"><div class="btn btn-green">Read more</div></a>
+
+           </section>
+
+
+     </div>
+
+  </div>
+
+     <!-- /.row -->
+    </div>
+  </div> <!-- /portfolio -->
+
+  <!-- ==================================== EXPRESSION WRAP ==================================== -->
+
+    <div class="expressionwrap" id="expressionwrap">
+      <div class="row">
+        <div class="col-md-12">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.
+        </div>
+      </div>
+    </div>
+
+    <!-- ==================================== ABOUT WRAP ==================================== -->
+
+    <div class="aboutwrap" id="aboutwrap">
+      <div class="container">
+        <div class="col-md-12 sub-title">
+          <h2 class="title-lv2 title-green">A propos</h2>
+          <p></p>
+        </div>
+
+      <div class="col-xs-12 col-md-8">
+        <img class="img-responsive img-center" src="http://placehold.it/500x250" alt="">
+      </div>
+
+      <div class="col-xs-12 col-md-4">
+        <div class="col-sm-12">
+            <h5 class="title-white title-lowercase">Photoshop</h5>
+            <div class="progress"><div class="progress-bar" style="width: 80%;"></div></div>
+            <h5 class="title-white title-lowercase">Illustrator</h5>
+            <div class="progress"><div class="progress-bar" style="width: 70%;"></div></div>
+            <h5 class="title-white title-lowercase">Flash</h5>
+            <div class="progress"><div class="progress-bar" style="width: 65%;"></div></div>
+            <h5 class="title-white title-lowercase">Premiere</h5>
+            <div class="progress"><div class="progress-bar" style="width: 60%;"></div></div>
+            <h5 class="title-white title-lowercase">AfterEfect</h5>
+            <div class="progress"><div class="progress-bar" style="width: 60%;"></div></div>
+            <h5 class="title-white title-lowercase">InDesign</h5>
+            <div class="progress"><div class="progress-bar" style="width: 50%;"></div></div>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-sm-12">
+        <p>
+          <h4 class="title-lv4 title-white" style="text-align:left;">Miinow CHan</h4>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+
+
+      </div>
+    </div>
+
+    <!-- ==================================== EXPRESSION WRAP ==================================== -->
+
+      <div class="expressionwrap" id="expressionwrap">
+        <div class="row">
+          <div class="col-md-12">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.
+          </div>
+        </div>
+      </div>
+
+
+      <!-- ==================================== CONTACT WRAP ==================================== -->
+    <div class="contactwrap" id="contactwrap">
+      <div class="container">
+        <div class="col-md-12 sub-title">
+          <h2 class="title-lv2 title-green">Contact</h2>
+          <p>
+
+          </p>
+        </div>
+
+        <div class="col-xs-12 col-md-6">
+          <form class="formContact" action="traitement.php" method="post">
+          <fieldset>
+            <label for="contactLastName">Nom</label><br/> <input type="text" name="contactLastName" maxlength="25" required>
+            <br/>
+            <label for="contactFirstName">Prenom</label><br/> <input type="text" name="contactFirstName" maxlength="25">
+          </fieldset>
+         </div>
+
+        <div class="col-xs-12 col-md-6">
+          <fieldset>
+            <label for="contactMobile">N° de telephone</label><br/> <input type="tel" name="contactMobile" maxlength="10">
+            <br/>
+            <label for="contactMail">Mail</label><br/> <input type="email" name="contactMail" maxlength="40" required>
+          </fieldset>
+        </div>
+
+        <div class="col-xs-12">
+          <fieldset>
+            <label for="contactSubjet">Titre</label><br/> <input type="text" name="contactSubject" maxlength="50" required>
+            <br/>
+            <label for="contactMessage">Message</label><br/>
+            <textarea name="contactMessage" id="contactMessage" required></textarea>
+            <br/>
+            <br/>
+            <button type="submit" name="contactButton" class="btn btn-green btn-lg" >Envoyer</button>
+          </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
+
+  <!-- ==================================== FOOTER WRAP ==================================== -->
+
+    <footer>
+      <div class="container">
+
+        <div class="col-sx-12 col-md-4">
+          <p>
+            <h4 class="title-lv4">Reseaux Sociaux</h4>
+            <a href="#"><img src="" alt="FB" /></a>
+            <a href="#"><img src="" alt="DA" /></a>
+            <a href="#"><img src="" alt="GIT" /></a>
+
+            <h4 class="title-lv4">Contact</h4>
+            PAPIN Hélène<br/>
+            <a href="mailto:miinowchan.creation@gmail.com">miinowchan.creation@gmail.com</a>
+          </p>
+        </div>
+
+        <div class="col-sx-12 col-md-4">
+          <p>
+            <h4 class="title-lv4">A propos de moi ~</h4>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+
+        <div class="col-sx-12 col-md-4">
+          <p>
+            <h4 class="title-lv4">C'est dit donc... c'est dit !</h4>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+
+      </div>
+    </footer>
+
+    <!-- JS BOOSTRAP -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- My JS -->
+    <!-- SPOILER -->
+    <script type="text/javascript">
+    function showSpoiler(obj){
+      var inner = obj.parentNode.getElementsByTagName("div")[0];
+      if (inner.style.display == "none")
+      inner.style.display = "";
+      else
+      inner.style.display = "none";
+    }
+    </script>
+
+
+    <script src="js/jquery-1.11.0.min.js" type="text/javascript"></script>
+
+    <script src="js/masonry.pkgd.min.js"></script> <!-- All script -->
+    <script src="js/imagesloaded.pkgd.min.js"></script> <!-- All script -->
+
+     <script>
+       $(function(){
+
+         var $container = $('.grid');
+
+         $container.imagesLoaded( function(){
+           $container.masonry({
+             itemSelector : 'li'
+           });
+         });
+
+       });
+     </script>
+
+    </body>
+  </html>
