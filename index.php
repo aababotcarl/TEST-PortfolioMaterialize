eff21ada35e034ce92bdb9138f55dfb6efd3193d
miihelene <helene.papin62@gmail.com>
2016-02-23 10:22:02 +0100

    Nav bar

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>MiinowChan</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+  <!--Import Google Icon Font-->
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="css/materialize.min.css">
+  <!-- main css -->
+  <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+  <!-- =============================== Header ===============================  -->
+<header>
+  <div class="container">
+    <img src="" alt="logo" />
+
+    <!-- =============================== Navigation ===============================  -->
+    <nav class="grey lighten-5">
+      <div class="navbar">
+        <a href="#" data-activates="mobile-demo" class="button-collapse right"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="#homewrap" class="page-scroll">Accueil</a></li>
+          <li><a href="#portfoliowrap" class="page-scroll">Portfolio</a></li>
+          <li><a href="#aboutwrap" class="page-scroll">À propos</a></li>
+          <li><a href="#contactwrap" class="page-scroll">Contact</a></li>
+        </ul>
+        <ul class="side-nav" id="mobile-demo">
+          <li><a href="#homewrap" class="page-scroll">Accueil</a></li>
+          <li><a href="#portfoliowrap" class="page-scroll">Portfolio</a></li>
+          <li><a href="#aboutwrap" class="page-scroll">À propos</a></li>
+          <li><a href="#contactwrap" class="page-scroll">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</header>
+  <!-- =============================== Contents ===============================  -->
+
+
+  <!-- Jquery -->
+  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+  <!-- Materialize -->
+  <script src="js/materialize.min.js"></script>
+  <!-- My Script -->
+  <script src="js/navbar.js"></script>
+
+
+
+</body>
+</html>
