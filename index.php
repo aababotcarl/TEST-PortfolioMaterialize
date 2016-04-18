e71fbf4a286e7cdef330fccfb984424ee0a015fc
miihelene <helene.papin62@gmail.com>
2016-04-18 09:03:26 +0200

    edit index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>MiinowChan</title>
+  <title>Mii CV</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
   <!--Import Google Icon Font-->
